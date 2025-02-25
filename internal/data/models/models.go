a9e0ce3684974b2c9f64e6c5e2ba47885fe22ed6
osamikoyo <v.pylayev@mail.ru>
2025-02-25 07:03:46 +0500

    add models

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,1 +1,19 @@
 package models
+
+import "time"
+
+type User struct{
+	ID uint64
+	Token string
+	Firstname string
+	Secondname string
+	Role string
+}
+
+type Document struct{
+	ID uint64 `json:"id"`
+	About string `json:"about"`
+	CreatedAt time.Time `json:"created_at"`
+	Content string `json:"content"`
+	Roles []string  `json:"roles"`
+}
