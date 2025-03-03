fb1e7892c1a1c0d64b01e1ac1651ae5554d81eae
osamikoyo <v.pylayev@mail.ru>
2025-03-03 13:56:38 +0500

    final main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/koyo-os/crm/internal/app"
+)
+
 func main() {
-	
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	app.Init().Run(ctx)
 }
