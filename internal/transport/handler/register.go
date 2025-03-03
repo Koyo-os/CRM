12b1096b57cade7e126f383657b025b36f86e969
osamikoyo <v.pylayev@mail.ru>
2025-03-03 05:43:21 +0500

    add  register logic

--- internal/transport/handler/register.go
+++ internal/transport/handler/register.go
@@ -1,7 +1,36 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/bytedance/sonic"
+	"github.com/koyo-os/crm/internal/data/models"
+)
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request){
-	
+	body, err := io.ReadAll(r.Body)
+    if err != nil{
+        http.Error(w, "cant read request body", http.StatusBadRequest)
+        return
+    }
+
+    var user models.User
+
+    if err := sonic.Unmarshal(body, &user);err != nil{
+        http.Error(w, "cant read request body", http.StatusBadRequest)
+        return
+    }
+
+    token, id, err := h.service.CreateUser(&user)
+    if err != nil{
+        http.Error(w, "cant create user", http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprint(w, map[string]string{
+        "id" : fmt.Sprintf("%d", id),
+        "token" : token,
+    })
 }
